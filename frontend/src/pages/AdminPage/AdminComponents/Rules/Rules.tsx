44aac0bc5b5c903de97e25a86caf1ad03ad265e8
Yevgeniy <leonov-y@gmail.com>
2023-08-21 20:16:40 +0600

    basic rule interface

--- frontend/src/pages/AdminPage/AdminComponents/Rules/Rules.tsx
+++ frontend/src/pages/AdminPage/AdminComponents/Rules/Rules.tsx
@@ -1,10 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from './Rules.module.css'
 
 const Rules: FC = () => {
+    const menu = ['Long', 'Short', 'Все'];
+    const [active, setActive] = useState(0); 
+
+    const changeActive = (id: number) => {
+        setActive(id);
+    }
     return(
         <div className={styles.rules}>
             <h1>Правила</h1>
+            <ul className={styles.menu}>
+                {menu.map((elem: string, index: number) => (
+                    <li key={elem} 
+                        className={active === index ? styles.menu_active : ''}
+                        onClick={() => changeActive(index)}>{elem}
+                    </li>
+                ))}
+            </ul>
+
+            
         </div>
     )
 }
