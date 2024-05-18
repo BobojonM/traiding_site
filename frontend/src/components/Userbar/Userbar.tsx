fb3dcfa8f1fbd3701ed8ad74fb8ffe592e10afb5
Yevgeniy <leonov-y@gmail.com>
2024-05-18 18:15:04 +0600

    navbar and userbar adopted

--- frontend/src/components/Userbar/Userbar.tsx
+++ frontend/src/components/Userbar/Userbar.tsx
@@ -1,37 +1,64 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useSelector } from "react-redux";
 import RootState from "../../models/RootState";
 import useAuth from "../../hooks/useAuth";
-import styles from './Userbar.module.css'
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import styles from './Userbar.module.css';
 import RegularButton from "../UI/Buttons/RegularButton";
+import { styled } from "@mui/material";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 interface UserbarInterface {
-    openSetting: () => void 
+    openSetting: () => void;
 }
 
-const Userbar: FC<UserbarInterface> = ({openSetting}) => {
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  }));
+
+const Userbar: FC<UserbarInterface> = ({ openSetting }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDrawer = () => setIsOpen(!isOpen);
+
     const user = useSelector((state: RootState) => state.toolkit.user);
-    const {logout} = useAuth();
-    const settings = () => {
-        openSetting();
-    }
-    return(
-        <div className={styles.pannel}>
-            <div className={styles.content}>
-                <h3>Добро пожаловать, {user.email}</h3>
-                <h4>{user.isActivated ? 'Аккаунт активирован' : 'Вам надо активировать аккаунт'}</h4>
+    const { logout } = useAuth();
 
-                {user.isAdmin 
-                ? (
-                    <div className={styles.settings}>
-                        <RegularButton action={settings}>Настройки</RegularButton>
+    return (
+        <>
+            <IconButton className={styles.iconButton} onClick={toggleDrawer}>
+                <AccountCircleIcon color="primary" className={styles.icon} fontSize="large"/>
+            </IconButton>
+
+            <Drawer color="primary" className={styles.drawer} anchor="right" open={isOpen} onClose={toggleDrawer}>
+                <DrawerHeader className={styles.header}>
+                    <IconButton onClick={() => setIsOpen(!isOpen)}>
+                        <ChevronLeftIcon color="primary" />
+                    </IconButton>
+                </DrawerHeader>
+                <div className={styles.pannel}>
+                    <div className={styles.content}>
+                        <h3>Добро пожаловать, {user.email}</h3>
+                        <h4>{user.isActivated ? 'Аккаунт активирован' : 'Вам надо активировать аккаунт'}</h4>
+
+                        {user.isAdmin && (
+                            <div className={styles.settings}>
+                                <RegularButton action={openSetting}>Настройки</RegularButton>
+                            </div>
+                        )}
+
+                        <button onClick={() => logout()} style={{ marginTop: '1rem' }}>Выйти</button>
                     </div>
-                ) : null}
-
-                <button onClick={() => logout()}>Выйти</button>
-            </div>
-        </div>
-    )
-}
+                </div>
+            </Drawer>
+        </>
+    );
+};
 
 export default Userbar;
